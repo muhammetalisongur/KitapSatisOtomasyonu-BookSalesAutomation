9c2587e4c3539b691b3e13b2f7a9c613b7a35ffb
Muhammet Ali Songur <muhammetalisongur@gmail.com>
2023-03-26 14:05:20 +0300

    YayinEvi

---
+++ KutuphanePlatformu/Areas/Admin/Controllers/KitapController.cs
@@ -0,0 +1,22 @@
+﻿using KutuphanePlatformu.Models.EntityFramework;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KutuphanePlatformu.Areas.Admin.Controllers
+{
+    public class KitapController : Controller
+    {
+        // GET: Admin/Kitap
+        KutuphanePlatformDbEntities db = new KutuphanePlatformDbEntities();
+        public ActionResult Index(int? sayfaNo)
+        {
+            int _sayfaNo = sayfaNo ?? 1;
+            var listele = db.Kitap.OrderByDescending(x=>x.Id).ToPagedList<Kitap>(_sayfaNo,10);
+            return View(listele);
+        }
+    }
+}
---
+++ KutuphanePlatformu/Areas/Admin/Controllers/YayinEviController.cs
@@ -0,0 +1,22 @@
+﻿using KutuphanePlatformu.Models.EntityFramework;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KutuphanePlatformu.Areas.Admin.Controllers
+{
+    public class YayinEviController : Controller
+    {
+        // GET: Admin/YayinEvi
+        KutuphanePlatformDbEntities db = new KutuphanePlatformDbEntities();
+        public ActionResult Index(int? sayfaNo)
+        {
+            int _sayfaNo = sayfaNo ?? 1;
+            var listele = db.YayinEvi.OrderByDescending(x => x.Id).ToPagedList(_sayfaNo, 10);
+            return View(listele);
+        }
+    }
+}
