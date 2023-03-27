006a6bfd9bf0ff17d2c9c8226adf7dbfe21a7b41
Muhammet Ali Songur <muhammetalisongur@gmail.com>
2023-03-27 09:09:10 +0300

    Update

--- KutuphanePlatformu/Areas/Admin/Controllers/YayinEviController.cs
+++ KutuphanePlatformu/Areas/Admin/Controllers/YayinEviController.cs
@@ -1,4 +1,5 @@
 ﻿using KutuphanePlatformu.Models.EntityFramework;
+using KutuphanePlatformu.ViewModels;
 using PagedList;
 using System;
 using System.Collections.Generic;
@@ -26,9 +27,60 @@ namespace KutuphanePlatformu.Areas.Admin.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Kaydet(int id)
+        public ActionResult Kaydet(YayinEvi yayinEvi)
         {
-            return View("YayinEviForm");
+            if (!ModelState.IsValid)
+            {
+                return View("YayinEviForm");
+            }
+            MesajViewModel model = new MesajViewModel();
+            if (yayinEvi.Id == 0)
+            {
+                db.YayinEvi.Add(yayinEvi);
+                model.Mesaj = yayinEvi.Ad + " başarıyle eklendi...";
+            }
+            else
+            {
+                var guncellenecekYayinEvi = db.YayinEvi.Find(yayinEvi.Id);
+                if (guncellenecekYayinEvi == null)
+                {
+                    return HttpNotFound();
+                }
+                guncellenecekYayinEvi.Ad = yayinEvi.Ad;
+                guncellenecekYayinEvi.Kitaplari = yayinEvi.Kitaplari;
+                guncellenecekYayinEvi.Detay = yayinEvi.Detay;
+                guncellenecekYayinEvi.Resim = yayinEvi.Resim;
+                model.Mesaj = yayinEvi.Ad + " başarıyla güncellendi...";    
+                model.Mesaj = yayinEvi.Kitaplari + " başarıyla güncellendi...";    
+                model.Mesaj = yayinEvi.Detay + " başarıyla güncellendi...";
+                model.Mesaj = yayinEvi.Resim + " başarıyla güncellendi...";
+
+            }
+            db.SaveChanges();
+            model.Status = true;
+            model.LinkText = "YayinEvi Listesi";
+            model.Url = "/Admin/Yazar";
+            return View("_Mesaj", model);
         }
+
+        public ActionResult Guncelle(int id)
+        {
+            var model = db.YayinEvi.Find(id);
+            if (model == null)
+                return HttpNotFound();
+            return View("YayinEviForm", model);
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var silinecekYayinEvi = db.YayinEvi.Find(id);
+            if (silinecekYayinEvi == null)
+                return HttpNotFound();
+            db.YayinEvi.Remove(silinecekYayinEvi);
+            db.SaveChanges();
+            return RedirectToAction("Index", "YayinEvi");
+
+        }
+
     }
 }
