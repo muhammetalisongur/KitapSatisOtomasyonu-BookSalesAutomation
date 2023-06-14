5eaeb30e7073a0578170f0e2b9d931bf2cd0559e
Muhammet Ali Songur <muhammetalisongur@gmail.com>
2023-06-14 23:54:23 +0300

    -

--- BookStore/Areas/Admin/Controllers/CategoryController.cs
+++ BookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -17,6 +17,7 @@ namespace KutuphanePlatformu.Areas.Admin.Controllers
             _categoryService = categoryService;
         }
 
+        [Route("/")]
         public ActionResult Index(int? SayfaNo)
         {
 
--- BookStore/Migrations/Configuration.cs
+++ BookStore/Migrations/Configuration.cs
@@ -1,18 +1,18 @@
-﻿namespace DataAccess.Migrations
+﻿namespace BookStore.Migrations
 {
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<DataAccess.Concrete.Context>
+    internal sealed class Configuration : DbMigrationsConfiguration<BookStore.Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(DataAccess.Concrete.Context context)
+        protected override void Seed(BookStore.Models.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
