a43c68b0ff9feced26b666edb3a4dd78fada8c82
muhammetalisongur <muhammetalisongur@MAS-PC>
2023-06-19 10:37:04 +0300

    authorUpdate

---
+++ DataAccess/Migrations/202306190724116_AuthorUpdate.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AuthorUpdate : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Authors", "AuthorImage", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Authors", "AuthorImage", c => c.String(nullable: false, maxLength: 250));
+        }
+    }
+}
