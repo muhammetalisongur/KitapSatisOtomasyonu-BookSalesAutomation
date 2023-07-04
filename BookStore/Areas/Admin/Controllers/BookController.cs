43eb071e09f75a80cfa1cfbcca2118d6cb27fd21
Muhammet Ali Songur <muhammetalisongur@gmail.com>
2023-07-04 13:32:04 +0300

    -

---
+++ BookStore/Areas/Admin/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿using BookStore.Areas.Admin.ViewModel;
+using Business.Concrete;
+using DataAccess.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Areas.Admin.Controllers
+{
+    [RouteArea("Admin")]
+    public class BookController : Controller
+    {
+        // GET: Admin/Book
+        BookManager manager = new BookManager(new EfBookDal());
+        MessageViewModel messageViewModel = new MessageViewModel();
+
+
+
+        [Route("Kitap")]
+        [Route("Kitap/Index")]
+        public ActionResult Index(int? SayfaNo)
+        {
+            int _sayfaNo = SayfaNo ?? 1;
+
+            var model = new List<Author>();
+
+          
+
+            return View(model.ToPagedList(_sayfaNo, 5));
+        }
+    }
+}
---
+++ Business/Concrete/BookManager.cs
@@ -0,0 +1,43 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class BookManager : IBookService
+    {
+       private readonly IBookDal _bookDal;
+        public BookManager(IBookDal bookDal)
+        {
+            _bookDal = bookDal;
+        }
+
+        public List<Book> GetAll()
+        {
+          return _bookDal.GetAll();
+        }
+
+        public Book GetById(int id)
+        {
+            return _bookDal.GetById(b => b.ID == id);
+        }
+        public void Add(Book book)
+        {
+            _bookDal.Add(book);
+        }
+
+        public void Delete(Book book)
+        {
+            _bookDal.Delete(book);
+        }
+        public void Update(Book book)
+        {
+            _bookDal.Update(book);
+        }
+    }
+}
