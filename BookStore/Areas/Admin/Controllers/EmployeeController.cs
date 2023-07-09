466cfd428c60bec5028499aa6b6f6783c3c555d3
muhammetalisongur <muhammetalisongur@gmail.com>
2023-07-09 14:23:00 +0300

    Employee

---
+++ BookStore/Areas/Admin/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Areas.Admin.Controllers
+{
+    [RouteArea("Admin")]
+    public class EmployeeController : Controller
+    {
+        // GET: Admin/Employee
+
+
+        [Route("Personel")]
+        [Route("Personel/Index")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Business/Concrete/EmployeeManager.cs
@@ -0,0 +1,48 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class EmployeeManager : IEmployeeService
+    {
+
+        private readonly IEmployeeDal _employeeDal;
+
+        public EmployeeManager(IEmployeeDal employeeDal)
+        {
+            _employeeDal = employeeDal;
+        }
+
+        public List<Employee> GetAll()
+        {
+            return _employeeDal.GetAll();
+        }
+
+        public Employee GetById(int id)
+        {
+            return _employeeDal.GetById(x => x.ID == id);
+        }
+
+        public void Add(Employee employee)
+        {
+            _employeeDal.Add(employee);
+        }
+        public void Update(Employee employee)
+        {
+            _employeeDal.Update(employee);
+        }
+        public void Delete(Employee employee)
+        {
+            _employeeDal.Delete(employee);
+        }
+
+
+
+    }
+}
